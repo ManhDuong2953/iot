44b413861037fe969a2cfa20c7e1dc2448bdab9a
ManhDuong2953 <=>
2024-03-05 20:41:47 +0700

    dfsd

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ app.use(bodyParser.json());
 
 let isLightOn = false;
 
-app.post('/', (req, res) => {
+app.post('/turn', (req, res) => {
   const { light } = req.body;
   isLightOn = light;
   console.log(`Light is turned ${isLightOn ? 'on' : 'off'}`);
   res.sendStatus(200);
 });
 
-app.get('/', function(req, res) {
+app.get('/test', function(req, res) {
   res.send("<h1>Light is turned</h1>");
 });
 
